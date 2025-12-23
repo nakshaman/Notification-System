30cf25ba4e3f9f0ff8ac22a720acccd45e29aea9
nakshaman <YOUR_GITHUB_EMAIL>
2025-12-24 01:15:56 +0530

    create dice app also with theme features

--- lib/app.dart
+++ lib/app.dart
@@ -1,32 +1,138 @@
 import 'package:buyit/noti_service.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class App extends StatelessWidget {
-  const App({super.key});
+class App extends StatefulWidget {
+  final Function onToggleTheme;
+  const App({super.key, required this.onToggleTheme});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int diceNumber = 1;
+  final random = Random();
+  void changeDice() {
+    setState(() {
+      diceNumber = random.nextInt(6) + 1;
+    });
+  }
+
+  ThemeData currentTheme = ThemeData.light();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[700],
-      body: Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.grey[500], // button color
-            foregroundColor: Colors.grey[300], // text color
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 14),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            elevation: 4,
+      appBar: AppBar(
+        title: Text(
+          'Dice Roller',
+          style: TextStyle(
+            color: Theme.of(context).brightness == Brightness.dark
+                ? Colors.white
+                : Colors.black,
           ),
-          onPressed: () {
-            NotiService().showNotification(
-              title: 'Hello Naksh',
-              body: 'Someone Messaged You nakshie! ',
-            );
-          },
-          child: const Text('Send Notification'),
         ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[700],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[500],
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      widget.onToggleTheme();
+                    },
+                    icon: Theme.of(context).brightness == Brightness.dark
+                        ? whiteThemeIcon()
+                        : blackThemeIcon(),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.grey[500],
+                  ),
+                  child: IconButton(
+                    onPressed: () {
+                      NotiService().showNotification(
+                        title: 'Dice rolled',
+                        body: 'Dice is rolled finally',
+                      );
+                    },
+                    icon: Icon(
+                      Icons.notification_add_rounded,
+                      color: Theme.of(context).brightness == Brightness.dark
+                          ? Colors.white
+                          : Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Divider(
+              thickness: 0.8,
+              color: Colors.grey,
+              radius: BorderRadius.circular(10),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.asset(
+                    'images/dice$diceNumber.png',
+                    color: Theme.of(context).brightness == Brightness.dark
+                        ? Colors.white
+                        : Colors.black,
+                    height: 200,
+                    width: 200,
+                    fit: BoxFit.contain,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      changeDice();
+                    },
+                    child: Text(
+                      'Change Dice',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                        color: Theme.of(context).brightness == Brightness.dark
+                            ? Colors.white
+                            : Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+Widget whiteThemeIcon() {
+  return Icon(Icons.light_mode, color: Colors.white);
+}
+
+Widget blackThemeIcon() {
+  return Icon(Icons.dark_mode, color: Colors.black);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,5 +5,41 @@ import 'package:flutter/material.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NotiService().initNotification();
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: App()));
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ThemeMode themeMode = ThemeMode.light;
+  void toggleTheme() {
+    setState(() {
+      themeMode == ThemeMode.light
+          ? themeMode = ThemeMode.dark
+          : themeMode = ThemeMode.light;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: themeMode,
+      debugShowCheckedModeBanner: false,
+      home: AnimatedTheme(
+        data: themeMode == ThemeMode.light
+            ? ThemeData.light()
+            : ThemeData.dark(),
+        duration: const Duration(milliseconds: 700),
+        curve: Curves.easeOutCubic,
+        child: App(onToggleTheme: toggleTheme),
+      ),
+    );
+  }
 }
